abd96205d4ab178e92dcdf1a64bfaebb38da94bd
Pedro <ruben@test.com>
2018-02-14 18:29:36 +0100

    NumeroFicheeros

---
+++ numerodeficheros.py
@@ -0,0 +1,14 @@
+﻿#Pedir para introducir una letra y listar los archivos com esa letra
+
+import os
+ficheros=os.listdir("c:\windows\system32")
+letra=raw_input("Introduz una letra: ")
+cuantos=0
+for x in ficheros:
+	if x.count(letra)>0:
+		#print x
+		cuantos=cuantos+1
+
+print " "
+print "Hay "+str(cuantos)+" ficheros con "+str(letra)
+	
